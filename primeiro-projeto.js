c9ddcc8dafb6661704bb7618a185864f29de2460
Rian Ramos <rian.90@hotmail.com.br>
2025-05-20 20:08:58 -0300

    Primeiras alteracoes no backend

--- primeiro-projeto.js
+++ primeiro-projeto.js
@@ -1,9 +1,16 @@
 const http = require("http")
-const port = 80;
+const port = 8080;
+
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Listagem de editoras'
+}
 
 const  server = http.createServer((requisition, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Curso de Node');
+    response.end(rotas[requisition.url]);
 })
 
 server.listen(port, () => {
